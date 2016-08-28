01be3231f4ab928b773b3a3b72fe2c929955a3d1
Miikka Koskinen <miikka.koskinen@iki.fi>
2016-08-28 23:34:40 +0300

    project.clj: Fill in metadata

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-pinkeys "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen plugin for pinning the PGP keys for your dependencies."
+  :url "https://github.com/miikka/lein-pinkeys"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[mvxcvi/clj-pgp "0.8.3"]]
+  :dependencies [[mvxcvi/clj-pgp "0.8.3" :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true)
